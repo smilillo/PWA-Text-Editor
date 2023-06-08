b3be0d45400b199f8f6d714a8cf4aa0808d37fcc
smilillo <sofiemilillo@gmail.com>
2023-06-08 16:01:06 -0400

    deleted todo comments from files

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,7 +26,6 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
 registerRoute(
     // Define callback function that will filter the requests
     ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-// export const putDb = async (content) => console.error('putDb not implemented');
+// Accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('Post to the datebase');
   const jateDb = await openDB('jate', 1);
@@ -24,8 +23,7 @@ export const putDb = async (content) => {
   console.log('Data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-// export const getDb = async () => console.error('getDb not implemented');
+// Get all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
